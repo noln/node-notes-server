de5de2b98f270765c7705d16ac7e1d1e03a4d958
Matt Fenlon <nolnefm@gmail.com>
2017-06-11 07:11:42 -0700

    Added server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+// server.js
+
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
