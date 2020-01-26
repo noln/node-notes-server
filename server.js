4e7680e22405f8748446d67792ddc42a6a600003
Matt Fenlon <nolnefm@gmail.com>
2020-01-26 10:40:47 -0800

    Allow port to be an ENV for Heroku to set

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db             = require('./config/db');
 
 const app            = express();
 
-const port = 8000;
+var port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
